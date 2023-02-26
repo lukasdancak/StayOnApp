8753cb369bc831bfedfab9b0f88ebaefde74a567
lukasdancak <lukas.dancak@gmail.com>
2023-02-26 16:01:33 +0100

    moving of cursor

--- src/main/java/sk/lukasdancak/stayonapp/controller/Controller.java
+++ src/main/java/sk/lukasdancak/stayonapp/controller/Controller.java
@@ -3,9 +3,11 @@ package sk.lukasdancak.stayonapp.controller;
 import sk.lukasdancak.stayonapp.model.Model;
 import sk.lukasdancak.stayonapp.view.GlobalUI;
 
+import java.awt.*;
 import java.sql.Time;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.sleep;
@@ -17,6 +19,15 @@ public class Controller {
     int seconds;
     LocalTime localTime;
     DateTimeFormatter dtf = DateTimeFormatter.ISO_LOCAL_TIME;
+    PointerInfo a; // position of mouse
+    Point b; //point of mouse posiiton, from PointerInfo
+    int x;
+    int y;
+    Robot r;
+
+    Random rand = new Random();
+
+
 
     public Controller(GlobalUI uiSolution, Model model) {
         this.uiSolution = uiSolution;
@@ -32,14 +43,37 @@ public class Controller {
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
+
             localTime=LocalTime.now();
             uiSolution.setLocalTimeLabel( localTime.withNano(0).format(dtf).toString() );
             if(isTimerOn){
+
                 seconds++;
                 uiSolution.setTimerLabel(Integer.toString(seconds));
+                moveMouseCursor();
             }
 
         }
+    }
+
+    private void moveMouseCursor() {
+        a = MouseInfo.getPointerInfo();
+        b = a.getLocation();
+        x = (int) b.getX();
+        y = (int) b.getY();
+
+        try {
+            r = new Robot();
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+        r.mouseMove(x+myRandomInt(), y + myRandomInt());
+    }
+
+    //generates random int number from (-100) to (+100)
+    private int myRandomInt(){
+        // master:  random.nextInt(max - min) + min ; my min =-100, my max=100
+        return rand.nextInt(100 +100)-100;
     }
 
     public void stopTimer() {
