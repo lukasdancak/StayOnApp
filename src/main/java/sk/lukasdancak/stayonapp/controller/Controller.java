a2a165a139f54e5f9a22a7e27705e52ba80d205c
lukasdancak <lukas.dancak@gmail.com>
2023-02-26 17:02:28 +0100

    duration counter changed

--- src/main/java/sk/lukasdancak/stayonapp/controller/Controller.java
+++ src/main/java/sk/lukasdancak/stayonapp/controller/Controller.java
@@ -4,9 +4,10 @@ import sk.lukasdancak.stayonapp.model.Model;
 import sk.lukasdancak.stayonapp.view.GlobalUI;
 
 import java.awt.*;
-import java.sql.Time;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -17,13 +18,17 @@ public class Controller {
     Model model;
     boolean isTimerOn = false;
     int seconds;
-    LocalTime localTime;
+    LocalDateTime actualDateTime;
+    LocalDateTime atStartOfCounter;
+
     DateTimeFormatter dtf = DateTimeFormatter.ISO_LOCAL_TIME;
+    DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("HH:mm:ss");
     PointerInfo a; // position of mouse
     Point b; //point of mouse posiiton, from PointerInfo
     int x;
     int y;
     Robot r;
+    long noOfSeconds;
 
     Random rand = new Random();
 
@@ -44,12 +49,13 @@ public class Controller {
                 throw new RuntimeException(e);
             }
 
-            localTime=LocalTime.now();
-            uiSolution.setLocalTimeLabel( localTime.withNano(0).format(dtf).toString() );
+            actualDateTime =LocalDateTime.now();
+            uiSolution.setLocalTimeLabel( actualDateTime.withNano(0).format(dtf).toString() );
             if(isTimerOn){
+                noOfSeconds = atStartOfCounter.until(actualDateTime, ChronoUnit.SECONDS);
 
                 seconds++;
-                uiSolution.setTimerLabel(Integer.toString(seconds));
+                uiSolution.setTimerLabel(String.format("%d:%02d:%02d", noOfSeconds / 3600, (noOfSeconds % 3600) / 60, (noOfSeconds % 60)));
                 moveMouseCursor();
             }
 
@@ -78,10 +84,12 @@ public class Controller {
 
     public void stopTimer() {
         isTimerOn=false;
+        atStartOfCounter=null;
     }
 
     public void startTimer() {
         isTimerOn=true;
+        atStartOfCounter= LocalDateTime.now();
         seconds=0;
     }
 }
